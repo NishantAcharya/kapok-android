3f11d9e5f349319bce199993301269f75e4228a6
APAChandra <36752758+APAChandra@users.noreply.github.com>
2019-05-30 19:54:46 +0530

    Added Firebase Register and Login

--- app/src/main/java/com/kapok/brianramirez/kapok/LogInActivity.java
+++ app/src/main/java/com/kapok/brianramirez/kapok/LogInActivity.java
@@ -2,11 +2,18 @@ package com.kapok.brianramirez.kapok;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import io.realm.ObjectServerError;
 import io.realm.Realm;
@@ -18,12 +25,15 @@ public class LogInActivity extends AppCompatActivity {
     EditText emailField;
     EditText passwordField;
     Button logInBtn;
+    private FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        mAuth = FirebaseAuth.getInstance();
 
         emailField = (EditText) findViewById(R.id.email_text_field);
         passwordField = (EditText) findViewById(R.id.password_text_field);
@@ -36,22 +46,42 @@ public class LogInActivity extends AppCompatActivity {
                 String password = passwordField.getText().toString();
 
                 if (isValidRequest(email, password)){
-                    SyncCredentials credentials = SyncCredentials.usernamePassword(email, password, false);
-                    SyncUser.logInAsync(credentials, Constants.AUTH_URL, new SyncUser.Callback<SyncUser>() {
-                        @Override
-                        public void onSuccess(SyncUser user) {
-                            // User is logged
-                            Toast.makeText(LogInActivity.this, "User Signed in successful", Toast.LENGTH_SHORT).show();
-                            Intent i = new Intent(LogInActivity.this, TeamWelcomeActivity.class);
-                            startActivity(i);
-                        }
+//                    SyncCredentials credentials = SyncCredentials.usernamePassword(email, password, false);
+//                    SyncUser.logInAsync(credentials, Constants.AUTH_URL, new SyncUser.Callback<SyncUser>() {
+//                        @Override
+//                        public void onSuccess(SyncUser user) {
+//                            // User is logged
+//                            Toast.makeText(LogInActivity.this, "User Signed in successful", Toast.LENGTH_SHORT).show();
+//                            Intent i = new Intent(LogInActivity.this, TeamWelcomeActivity.class);
+//                            startActivity(i);
+//                        }
+//
+//                        @Override
+//                        public void onError(ObjectServerError error) {
+//                            System.out.println(error);
+//                            Toast.makeText(LogInActivity.this, "Log in failed....     :(", Toast.LENGTH_SHORT).show();
+//                        }
+//                    });
 
-                        @Override
-                        public void onError(ObjectServerError error) {
-                            System.out.println(error);
-                            Toast.makeText(LogInActivity.this, "Log in failed....     :(", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LogInActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Toast.makeText(LogInActivity.this, "User Signed in successful", Toast.LENGTH_SHORT).show();
+                                        Intent i = new Intent(LogInActivity.this, TeamWelcomeActivity.class);
+                                        startActivity(i);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(LogInActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    // ...
+                                }
+                            });
                 } else {
                     //TODO: show error saying that username and pass are too short or empty
                 }
--- app/src/main/java/com/kapok/brianramirez/kapok/RegisterActivity.java
+++ app/src/main/java/com/kapok/brianramirez/kapok/RegisterActivity.java
@@ -2,11 +2,18 @@ package com.kapok.brianramirez.kapok;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import io.realm.ObjectServerError;
 import io.realm.Realm;
@@ -19,11 +26,13 @@ public class RegisterActivity extends AppCompatActivity {
     EditText passwordField;
     EditText confirmPassField;
     Button registerBtn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
 
 
         emailField = (EditText)findViewById(R.id.email_text_field);
@@ -43,19 +52,34 @@ public class RegisterActivity extends AppCompatActivity {
                 String confirmPass = confirmPassField.getText().toString();
 
                 if (isValidRequest(email, password, confirmPass)){
-                    SyncCredentials credentials = SyncCredentials.usernamePassword(email, password, true);
-                    SyncUser.logInAsync(credentials, Constants.AUTH_URL, new SyncUser.Callback<SyncUser>() {
-                        @Override
-                        public void onSuccess(SyncUser user) {
-                            goToProfileSetup();
-                        }
-
-                        @Override
-                        public void onError(ObjectServerError error) {
-                            System.out.println(error.getLocalizedMessage());
-                            Toast.makeText(RegisterActivity.this, "Registering  failed....     :(", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+//                    SyncCredentials credentials = SyncCredentials.usernamePassword(email, password, true);
+//                    SyncUser.logInAsync(credentials, Constants.AUTH_URL, new SyncUser.Callback<SyncUser>() {
+//                        @Override
+//                        public void onSuccess(SyncUser user) {
+//                            goToProfileSetup();
+//                        }
+//
+//                        @Override
+//                        public void onError(ObjectServerError error) {
+//                            System.out.println(error.getLocalizedMessage());
+//                            Toast.makeText(RegisterActivity.this, "Registering  failed....     :(", Toast.LENGTH_SHORT).show();
+//                        }
+//                    });
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        goToProfileSetup();
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
 
                 } else {
                     //TODO: show error saying pass don't match or email is too short
