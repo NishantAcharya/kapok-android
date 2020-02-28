ae3b4b72b1a81b515eca2a3b008c104aa2b2f17d
APAChandra <aparimitchan@umass.edu>
2020-02-28 04:51:31 -0500

    Service

---
+++ app/src/main/java/com/kapok/brianramirez/kapok/DatabaseListener.java
@@ -0,0 +1,79 @@
+package com.kapok.brianramirez.kapok;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.ArrayList;
+
+import static android.support.constraint.Constraints.TAG;
+
+public class DatabaseListener extends Service {
+    private FirebaseAuth mAuth;
+    private DocumentSnapshot prevUserSnap;
+    private DocumentSnapshot prevTeamSnap;
+    private String team;
+
+    @Override
+    public void onCreate() {
+        mAuth = Database.mAuth;
+        String currentUser = mAuth.getCurrentUser().getEmail();
+        FirebaseFirestore db = Database.db;
+        DocumentReference docRef = db.collection("Profiles").document(currentUser);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    // Document found in the offline cache
+                    prevUserSnap = task.getResult();
+                }
+            }
+        });
+
+
+
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                @Nullable FirebaseFirestoreException e) {
+
+                if (snapshot != null && snapshot.exists()) {
+                    docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                if (document.exists()) {
+                                    ArrayList<String> requests = (ArrayList<String>)document.get("requests");
+                                    ArrayList<String> prevrequests = (ArrayList<String>)prevUserSnap.get("request");
+                                    if(requests.size()>prevrequests.size()){
+                                        //make notification
+                                    }
+                                    prevUserSnap = document;
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
